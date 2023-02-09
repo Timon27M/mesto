ccda2715cc645a36614d8f4543ac35e55d8bebdf
Timofey Pichugin <tpich87@gmail.com>
2023-02-09 19:04:40 +0300

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { Card } from "../scripts/card.js";
+import { Card } from "./Card.js";
 import {  FormValidator } from "./FormValidator.js";
 import { objectSetting } from "./objectSetting.js";
 import { initialCards } from "./cards.js"
