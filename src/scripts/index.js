152105760ffb1eca58927ff68c32d35adf5bcade
Timofey Pichugin <tpich87@gmail.com>
2023-02-25 12:28:24 +0300

    feat: созданы новые классы

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,11 @@
-import Card from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
+import Card from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
 import { objectSetting } from "./objectSetting.js";
 import { initialCards } from "./cards.js";
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
 
 
 const popupEditProfile = document.querySelector(".popup_edit-profile");
