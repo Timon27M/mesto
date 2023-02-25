244e80621ec17411d099c0cc30700e688ae42d82
Timofey Pichugin <tpich87@gmail.com>
2023-02-25 17:44:13 +0300

    feat: добавлен webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import '../pages/index.css';
+
 import Card from "../components/Card.js";
 import { FormValidator } from "../components/FormValidator.js";
 import { objectSetting } from "./objectSetting.js";
@@ -7,17 +9,15 @@ import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 
-
-const popupEditProfile = document.querySelector(".popup_edit-profile");
-const popupFormEditProfile = popupEditProfile.querySelector(".popup__form");
-const popupInputName = popupEditProfile.querySelector(".popup__input_info_name");
-const popupInputDescription = popupEditProfile.querySelector(".popup__input_info_description");
-const popupAddCard = document.querySelector(".popup_add-card");
-const popupFormAddCard = popupAddCard.querySelector(".popup__form");
-const profile = document.querySelector(".profile");
-const profileEditButton = profile.querySelector(".profile__edit-button");
-const profileAddButton = profile.querySelector(".profile__add-button");
-const sectionCards = document.querySelector(".elements");
+import {  
+  popupFormEditProfile,
+  popupInputName,
+  popupInputDescription,
+  popupFormAddCard,
+  profileEditButton,
+  profileAddButton,
+  sectionCards,
+} from '../utils/constans.js'
 
 
 // добавление валидации форм попапа редактирования профиля
---
+++ src/utils/constans.js
@@ -0,0 +1,20 @@
+const popupEditProfile = document.querySelector(".popup_edit-profile");
+const popupFormEditProfile = popupEditProfile.querySelector(".popup__form");
+const popupInputName = popupEditProfile.querySelector(".popup__input_info_name");
+const popupInputDescription = popupEditProfile.querySelector(".popup__input_info_description");
+const popupAddCard = document.querySelector(".popup_add-card");
+const popupFormAddCard = popupAddCard.querySelector(".popup__form");
+const profile = document.querySelector(".profile");
+const profileEditButton = profile.querySelector(".profile__edit-button");
+const profileAddButton = profile.querySelector(".profile__add-button");
+const sectionCards = document.querySelector(".elements");
+
+export {  
+    popupFormEditProfile,
+    popupInputName,
+    popupInputDescription,
+    popupFormAddCard,
+    profileEditButton,
+    profileAddButton,
+    sectionCards,
+ }
